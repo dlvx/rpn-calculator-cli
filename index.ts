ee543dbb0040d22a0f37c24377820aae30f07822
José Del Valle <dell2793@gmail.com>
2022-11-11 07:51:56 -0600

    Adds colors to the terminal output using chalk. Adds set of instructions

--- index.ts
+++ index.ts
@@ -1,7 +1,32 @@
 import inquirer from 'inquirer';
+import chalk from 'chalk';
 import { calculate } from './utils/operations.js';
 
-console.log("Hello!")
+const log = console.log;
+log(`
+${chalk.bold.cyan('Welcome to the RPN CLI!')}
+
+${chalk.cyan('Instructions:')}
+- Use reverse polish notation to input your operands and operators,
+  you can input an entire set like this, using a space as delimiter:
+
+  ${chalk.yellow('5 5 5 8 + + -')}
+
+  
+- You can also input operand by operand, hitting enter in between. 
+  The operands will be added to a stack for later use by an operator.
+  Calculations will execute once you enter an operator.
+- In both scenarios, an operator needs two preceeding operands, otherwise it will be disregarded.
+- The allowed operators are:
+
+  ${chalk.yellow('+ - / *')}
+
+- An empty input will be interpreted as a ${chalk.bold.yellow('0')}.
+- Type ${chalk.bold.yellow('c')} if you want to clear your stack.
+- Type ${chalk.bold.yellow('q')} if you want to quit.
+
+${chalk.bold.cyan('Have fun!')}
+`);
 
 const questions = [
   {
@@ -23,23 +48,21 @@ const questions = [
   },
 ]
 
-const output: string[] = [];
 let operands: number[] = [];
 
 function ask() {
   inquirer.prompt(questions).then((answers: any) => {
     const input = answers.input;
-    output.push(input);
 
     if(input === 'q') {
-      console.log('Good bye!');
+      console.log(chalk.bold.cyan('Good bye!'));
     } else if(input === 'c') { 
       operands = [];
-      console.log('Cleared!')
+      console.log(chalk.bold.cyan('Cleared!'))
       ask();
     } else {
       operands = calculate(input, operands)
-      console.log(operands[operands.length - 1]);
+      console.log(chalk.bold.yellow(operands[operands.length - 1]));
       ask();
     }
   });
